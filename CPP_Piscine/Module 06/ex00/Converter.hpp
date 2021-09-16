4542767af275159270375f1f9cd3718dbcf2db90
Mapes Clown <mitya.avadyaev@gmail.com>
2021-09-16 22:24:28 +0300

    Start Module 06

---
+++ CPP_Piscine/Module 06/ex00/Converter.hpp
@@ -0,0 +1,19 @@
+#ifndef CONVERTER_HPP
+#define CONVERTER_HPP 
+
+class Converter {
+
+public:
+
+    Converter();
+    Converter(const Converter &other);
+    ~Converter();
+    Converter& operator=(const Converter &other);
+    
+private:
+
+    
+
+};
+
+#endif
